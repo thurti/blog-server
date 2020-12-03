e69cdfa86cfb86bd5cdd7f4aab8f8b984df628a1
Thomas Hurtig <thurtig.email@gmail.com>
2020-12-03 15:25:54 +0100

    define config types and add ssr vars

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,21 @@
-export default {
-    PORT: process.env.PORT || 3000,
+const config: {
+    PORT: number,
+    POSTS: string,
+    ORIGIN: string,
+    STATIC: string,
+    TEMPLATES: string,
+    FRONTEND_INDEX: string,
+    FRONTEND_APP: string,
+    FRONTEND_ROUTES: string,
+  } = {
+    PORT: parseInt(process.env.PORT as string) || 3000,
     POSTS: process.env.POSTS || `${__dirname}/posts`,
     ORIGIN: process.env.ORIGIN || "http://localhost",
     STATIC: process.env.STATIC || `${__dirname}/static`,
     TEMPLATES: process.env.TEMPLATES || `${__dirname}/templates`,
-    FRONTEND: process.env.FRONTEND || `${__dirname}/frontend`,
-    FRONTEND_INDEX: process.env.FRONTEND_INDEX || `${__dirname}/frontend/public/index.html`,
-    FRONTEND_SINGLE: process.env.FRONTEND_SINGLE || `${__dirname}/frontend/src/views/Single.svelte`
+    FRONTEND_INDEX: process.env.FRONTEND_INDEX || '',
+    FRONTEND_APP: process.env.FRONTEND_APP || '',
+    FRONTEND_ROUTES: process.env.FRONTEND_ROUTES || '',
 };
+
+export default config;
